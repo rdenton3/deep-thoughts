3245bd5d41f7eed0016681cce71cf1d022ab31c9
Rachel Denton <rdenton3@terpmail.umd.edu>
2022-04-12 10:53:16 -0400

    client side routing set up

--- server/models/Reaction.js
+++ server/models/Reaction.js
@@ -1,5 +1,5 @@
 const { Schema } = require('mongoose');
-const dateFormat = require('../server/utils/dateFormat');
+const dateFormat = require('../utils/dateFormat');
 
 const reactionSchema = new Schema(
   {
--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const { authMiddleware } = require('./utils/auth');
+const path = require('path');
 // import ApolloServer
 const { ApolloServer } = require('apollo-server-express');
 
 // import our typeDefs and resolvers
-const { typeDefs, resolvers } = require('../schemas');
-const db = require('../config/connection');
+const { typeDefs, resolvers } = require('./schemas');
+const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -33,6 +34,15 @@ startServer();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Serve up static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once('open', () => {
   app.listen(PORT, () => {
